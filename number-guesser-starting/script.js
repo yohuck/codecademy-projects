6ad89cb23300e8f67227827348bf52c2a0dee124
Eric Lake <ericallenlake@gmail.com>
2022-04-04 16:09:32 -0600

    upload-solution
    uploading my solution to the number guesser project

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random()*10);
+
+
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+    let humanDist = Math.abs(humanGuess - secretTarget);
+    let computerDist = Math.abs(computerGuess - secretTarget);
+    if (humanDist <= computerDist) return true;
+    else return false;
+}
+
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    } else console.log('invalid winner');
+}
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
