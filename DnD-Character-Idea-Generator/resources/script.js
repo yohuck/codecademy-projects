258221590ed3906102cbfb385c9bd74ed32650de
Eric Lake <ericallenlake@gmail.com>
2022-04-26 10:12:38 -0600

    Update script.js

---
+++ DnD-Character-Idea-Generator/resources/script.js
@@ -0,0 +1,5 @@
+ const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+
+ const races = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling']
+
+const things = ['Dresses in drag at every opportunity', 'Has a terrible gambling problem', 'Can never remember names', 'Believes they are a teenage from the Earth year 2022 trapped in their Dungeons & Dragoons game ', 'is constantly hungry', "Can't pronunce locations correctly", 'is terrified of magic', 'tries to befriend every horse they encounter']
