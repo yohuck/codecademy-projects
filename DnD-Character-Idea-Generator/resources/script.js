b7077b7caac286986b51c488df364542b8b1e232
Eric Lake <ericallenlake@gmail.com>
2022-04-26 21:42:33 -0600

    adds full functionality v1
    version 1 functionality completed.

--- DnD-Character-Idea-Generator/resources/script.js
+++ DnD-Character-Idea-Generator/resources/script.js
@@ -58,14 +58,26 @@ button.addEventListener('click', function(event) {
         classSlide.setAttribute('style', 'transform: translate(0%,0');
         const thingSlide = document.getElementById('thing');
         thingSlide.setAttribute('style', 'transform: translate(-50%,0');
+        const body = document.getElementById('body')
+        body.setAttribute('style', "background-color: var(--dnd-orange)")
         state = false;
         } else {
+            const raceUpdate = document.getElementById('raceTag-2');
+        raceUpdate.textContent = thisCharacter[1];
+        const classUpdate = document.getElementById('classTag-2');
+        classUpdate.textContent = thisCharacter[0];
+        const thingUpdate = document.getElementById('thingTag-2');
+        thingUpdate.outerHTML = `<p id='thingTag-2'>${thisCharacter[2]}</p>`;
+
+
             const raceSlide = document.getElementById('race');
         raceSlide.setAttribute('style', 'transform: translate(0%,0');
         const classSlide = document.getElementById('class');
         classSlide.setAttribute('style', 'transform: translate(-50%,0');
         const thingSlide = document.getElementById('thing');
         thingSlide.style.transform = 'translate(0%,0)';
+        const body = document.getElementById('body')
+        body.setAttribute('style', "background-color: var(--dnd-blue)")
         state = true;
         }
     }
